35e19e849b82b7eec726a521b286e49399015ba7
JustinLien0805 <6998dragoan@gmail.com>
2022-09-24 20:33:38 +0800

    Initial Commit

---
+++ swift-weather-app/swift_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_weather_appApp.swift
+//  swift-weather-app
+//
+//  Created by 練家辰 on 2022/9/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
